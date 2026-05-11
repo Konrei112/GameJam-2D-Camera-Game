a8d9b5ae54a571289423658c8d69c7cceecb5526
grtandoc <grtandoc@feualabang.edu.ph>
2026-05-11 16:36:17 +0800

    MainMenu + animations

---
+++ Assets/MainMenu/MainMenu_UIManager.cs
@@ -0,0 +1,29 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MainMenu_UIManager : MonoBehaviour
+{
+    public GameObject settingsPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+}
