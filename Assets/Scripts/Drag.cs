493cc360d62d93cc6bced28d1f7ce3afe486f6bd
grtandoc <grtandoc@feualabang.edu.ph>
2026-05-11 15:30:40 +0800

    Lens Done :D

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -3,19 +3,42 @@ using UnityEngine;
 public class Drag : MonoBehaviour
 {
     [SerializeField] private bool isDragging = false;
+    public Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         if (isDragging)
         {
             transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            animator.SetBool("isDragging", true);
+  
         }
     }
 
     private void OnMouseDown()
     {
-        isDragging = !isDragging;
+        isDragging = true;
     }
 
+    private void OnMouseUp()
+    {
+        isDragging = false;
+        animator.SetBool("isDragging", false);
+    }
+
+    private void OnMouseEnter()
+    {
+        animator.SetBool("IsHovering", true);
+    }
+
+    private void OnMouseExit()
+    {
+        animator.SetBool("IsHovering", false);
+    }
 
 }
