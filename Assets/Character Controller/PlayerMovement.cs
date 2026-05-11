9f2cc87245261c6fc131d158d6200c405dc04665
ALEXANDER JOHN MALABANAN <acmalabanan@feualabang.edu.ph>
2026-05-11 14:36:26 +0800

    Added Physics Material

--- Assets/Character Controller/PlayerMovement.cs
+++ Assets/Character Controller/PlayerMovement.cs
@@ -2,15 +2,58 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")]
+    public PlayerMovementStats MoveStats;
+    [SerializeField] private Collider2D _feetColl;
+    [SerializeField] private Collider2D _bodyColl;
+
+    private Rigidbody2D _rb;
+
+    //movement vars
+    private Vector2 _moveVelocity;
+    private bool _IsFacingRight;
+
+    private RaycastHit2D _groundhit;
+    private RaycastHit2D _headhit;
+    private bool _isGrounded;
+    private bool _bumpedHead;
+
+    private void Awake()
     {
-        
+        _IsFacingRight = true;
+
+        _rb= GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    #region Movement
+
+    private void Move(float acceleration, float deceleration, Vector2 moveInput)
     {
-        
+        if (moveInput != Vector2.zero)
+        {
+            Vector2 targetVelocity = Vector2.zero;
+            if (InputManager.RunIsHeld)
+            {
+                targetVelocity = new Vector2(moveInput.x, 0f) * MoveStats.MaxRunSpeed;
+            }
+            else { targetVelocity = new Vector2(moveInput.x, 0f) * MoveStats.MaxWalkSpeed; }
+
+            _moveVelocity = Vector2.Lerp(_moveVelocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+            _rb.linearVelocity = new Vector2(_moveVelocity.x, _rb.linearVelocity.y);// Problem
+        }
+        else if (moveInput == Vector2.zero)
+        {
+            _moveVelocity= Vector2.Lerp(_moveVelocity, Vector2.zero, deceleration * Time.fixedDeltaTime);
+            _rb.linearVelocity = new Vector2(_moveVelocity.x, _rb.linearVelocity.y);// Problem
+        }
     }
+
+    private void TurnCheck(Vector2 moveInput)
+
 }
+
+
+
