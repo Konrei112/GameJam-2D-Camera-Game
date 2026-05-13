549ddac45605bf97b27025d55c33992c09fcfe04
lcmaliglig <lcmaliglig@feualabang.edu.ph>
2026-05-13 09:49:20 +0800

    Lab Updated v.2

--- Assets/TestFOlder/TestExitScene.cs
+++ Assets/TestFOlder/TestExitScene.cs
@@ -24,7 +24,7 @@ public class TestExitScene : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Player"))
+        if (collision.collider.CompareTag("PlayerMain"))
         {
             LoadNextScene();
         }
