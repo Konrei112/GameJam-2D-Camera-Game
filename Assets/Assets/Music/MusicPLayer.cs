fbb778174c4e015d3b9f0db1ba096a1065a373db
grtandoc <grtandoc@feualabang.edu.ph>
2026-05-13 10:33:44 +0800

    Update

--- Assets/Assets/Music/MusicPLayer.cs
+++ Assets/Assets/Music/MusicPLayer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class MusicPLayer : MonoBehaviour
 {
 
-    public MusicPLayer instance;
+    public static MusicPLayer instance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
